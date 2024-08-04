4371dc340a4dbcf1bea5915f8f73e2e412febadb
R-Swetha1103 <swetharagupathy1103@gmail.com>
2024-08-05 00:38:27 +0530

    dom form

---
+++ script.js
@@ -0,0 +1,16 @@
+var formSubmit=document.querySelector('#form')
+
+formSubmit.addEventListener('submit',(a) => {
+a.preventDefault()
+var firstName = document.querySelector('#firstName')
+var lastName = document.querySelector('#lastName')
+var Address = document.querySelector('#address')
+var pincode = document.querySelector('#pincode')
+var gender = document.querySelector('#gender')
+var food = document.querySelector('#food')
+var state = document.querySelector('#state')
+var country = document.querySelector('#country')
+
+var array = [firstName,lastName,address,pincode,gender,food,state,country]
+
+})
